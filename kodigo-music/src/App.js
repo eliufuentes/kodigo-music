dfb1a6b30ac7795260118dbee698a0951810a208
eliulandaverde <eliulandverde@gmail.com>
2025-01-23 11:26:13 -0800

    Fix: Bootstrap and basic React structure

--- kodigo-music/src/App.js
+++ kodigo-music/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Explore from './pages/Explore';
-import ContactForm from './pages/ContactForm';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/contact" element={<ContactForm />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="container text-center mt-5">
+      <h1>Welcome to Kodigo Music</h1>
+      <p className="lead">This is a sample application with Bootstrap styles</p>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
   );
 }
 
